dcc5b4d34b963b74c65c35164e859368f7aa37ed
schnehowebking <schnehowebking@gmail.com>
2024-05-11 23:46:09 +0600

    [MERGING]- Merging from mamun Branch

--- schools/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class School(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.TextField()
-    contact_info = models.CharField(max_length=50)
