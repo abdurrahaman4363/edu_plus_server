58dee23d2598631563e5b3bcbb324de86852f44b
schnehowebking <schnehowebking@gmail.com>
2024-05-12 23:37:53 +0600

    [ADDED]-User email

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,14 +3,16 @@ from django.contrib.auth.models import User
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    email = serializers.EmailField()
 
     class Meta:
         model = User
-        fields = ["__all__"]
+        fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
+            email=validated_data['email'],
             password=validated_data['password']
         )
         return user
