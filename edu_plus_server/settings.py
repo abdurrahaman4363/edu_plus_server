99d59afedbe5acb75898bbf10eb17ab53939d815
schnehowebking <schnehowebking@gmail.com>
2024-05-12 23:13:31 +0600

    [ADDED]-Added CORS Orign

--- edu_plus_server/settings.py
+++ edu_plus_server/settings.py
@@ -72,6 +72,7 @@ ROOT_URLCONF = "edu_plus_server.urls"
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
+    'https://edu-plus-server.onrender.com'
     # Add other allowed origins as needed
 ]
 CORS_ALLOW_METHODS = [
