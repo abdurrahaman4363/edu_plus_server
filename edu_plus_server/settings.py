22866deac8eca90569003431a9161124b6df88ac
schnehowebking <schnehowebking@gmail.com>
2024-05-12 14:40:15 +0600

    [ADD]- Add classes and attendences app

--- edu_plus_server/settings.py
+++ edu_plus_server/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     'rest_framework.authtoken',
-    'django_filters'
+    'django_filters',
     "employee",
     "teachers",
     'Students',
