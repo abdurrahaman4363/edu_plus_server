dcbbf5269c6d4ee2f46917ea7c250baa40465e3e
schnehowebking <schnehowebking@gmail.com>
2024-05-12 22:51:58 +0600

    [ADDED]- added codes to newdevelopement branch

--- grade/models.py
+++ grade/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from students.models import Student
 
 class Grade(models.Model):
-    drade_ID = models.AutoField(primary_key=True)
+    grade_ID = models.AutoField(primary_key=True)
     student_ID = models.ForeignKey(Student, on_delete=models.CASCADE)
     grade_Name = models.CharField(max_length=100)
     grade_Point = models.DecimalField(max_digits=5, decimal_places=2)
