98b0001e3679f6e250f50ac95f278f03536f4d39
SoumenBaral <shumen.sb@gmail.com>
2024-05-12 15:12:55 +0600

    solve assignment issue

--- Assignment/views.py
+++ Assignment/views.py
@@ -3,6 +3,6 @@ from rest_framework import viewsets
 from . import models, serializers
 
 # Create your views here.
-class TeacherViewSet(viewsets.ModelViewSet):
+class AssignmentViewSet(viewsets.ModelViewSet):
     queryset = models.Assignment.objects.all()
     serializer_class = serializers.AssignmentSerializer
